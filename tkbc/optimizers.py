fcba20355d7108ef1cff3b4272ef6a046578b123
Jiang Li <115767656+dellixx@users.noreply.github.com>
2024-04-13 17:18:35 +0800

    Update optimizers.py

--- tkbc/optimizers.py
+++ tkbc/optimizers.py
@@ -7,7 +7,6 @@ from models import TKBCModel
 from regularizers import Regularizer
 from datasets import TemporalDataset
 
-
 class TKBCOptimizer(object):
     def __init__(
             self, model: TKBCModel,
@@ -22,6 +21,7 @@ class TKBCOptimizer(object):
         self.batch_size = batch_size
         self.verbose = verbose
 
+
     def epoch(self, examples: torch.LongTensor):
         actual_examples = examples[torch.randperm(examples.shape[0]), :]
         loss = nn.CrossEntropyLoss(reduction='mean')
@@ -32,23 +32,27 @@ class TKBCOptimizer(object):
                 input_batch = actual_examples[
                     b_begin:b_begin + self.batch_size
                 ].cuda()
-                predictions, factors, time = self.model.forward(input_batch)
+
+                predictions, factors, time, time_phase = self.model.forward(input_batch)
                 truth = input_batch[:, 2]
 
                 l_fit = loss(predictions, truth)
                 l_reg = self.emb_regularizer.forward(factors)
                 l_time = torch.zeros_like(l_reg)
                 if time is not None:
-                    l_time = self.temporal_regularizer.forward(time)
-                l = l_fit + l_reg + l_time
+                    l_time = self.temporal_regularizer.forward(time, time_phase)
 
+                l = l_fit + l_reg + l_time
+                
                 self.optimizer.zero_grad()
                 l.backward()
                 self.optimizer.step()
                 b_begin += self.batch_size
                 bar.update(input_batch.shape[0])
+                print('loss={},reg={},cont={}'.format(l_fit.item(),l_reg.item(),l_time.item()))
                 bar.set_postfix(
                     loss=f'{l_fit.item():.0f}',
                     reg=f'{l_reg.item():.0f}',
                     cont=f'{l_time.item():.0f}'
                 )
+
